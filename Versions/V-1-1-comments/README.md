13a61591ae27356e5f201178a50cd11fa589aeaf
ThiagoMassenoMaciel <maciel72210764@gmail.com>
2023-12-28 18:10:59 -0300

    Versions - V 1.1 -comments

---
+++ Versions/V-1-1-comments/README.md
@@ -0,0 +1,4 @@
+# Projeto-09-V-1-1-comments-front-end.github.io
+Version 1.1 comments
+
+# Link [here]( https://thiagomassenomaciel.github.io/Projeto-09-V-1-1-comments-front-end.github.io/)
---
+++ Versions/V-1-1-comments/script.js
@@ -0,0 +1,43 @@
+//variablea
+const form = document.querySelector("form")
+const inputWeight = document.querySelector("#input-weight")
+const inputHeight = document.querySelector("#input-height")
+
+const ModalWrapper = document.querySelector(".modal-wrapper")
+const ShowResult = document.querySelector(".modal .tittle span")
+const buttonClose = document.querySelector(".modal button.close")
+//.on events
+form.onsubmit = function(e){
+  
+  //when this event happens the pattern's to submit the form and update the page. 
+  //When the button is clicked automatcly will submit the form 
+  //BREAK that ------> evitar Padrão      event.preventDefault()
+  e.preventDefault()
+  console.log(e);
+
+  const Weight = inputWeight.value ;
+  const Height = inputHeight.value ;
+
+  console.log("Executou a função")
+  console.log("getted :", Weight, Height)
+
+  calculating = calcIMC(Height,Weight)
+
+  console.log(calculating);
+  
+  ShowResult.innerText = calculating;
+  ModalWrapper.classList.add("open");
+
+}
+
+buttonClose.onclick = CloseModal;
+
+
+function CloseModal(){
+  ModalWrapper.classList.remove("open");
+}
+
+const calcIMC= (h,w)=>{
+  const result = (w /(h/100)**2)
+  return result.toFixed(2)
+}
